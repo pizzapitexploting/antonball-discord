68ec6436c68d71d0c380577c3d51e7f2631f4847
pizzapitexploting <107763001+pizzapitexploting@users.noreply.github.com>
2024-11-11 15:43:39 -0500

    Delete config directory

--- config/vite.mjs
+++
@@ -1,8 +0,0 @@
-import { DiscordProxy } from '@robojs/patch'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react(), DiscordProxy.Vite()]
-})
